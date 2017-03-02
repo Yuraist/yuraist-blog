5406b5be8843de95f34e97f99ebf9c8eddf2efb0
Yuri Istomin <yura.ist@icloud.com>
2017-03-02 16:06:41 +0000

    Set DEBUG = False

--- yuraist/settings.py
+++ yuraist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=pybeoglt05yjcso0#ym&=r&gesrokrntliq$z8964)tna+4z!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
