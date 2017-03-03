c2f1f411322e7373fd7fdb6aeaaf5808db47b7d8
Yuri Istomin <yura.ist@icloud.com>
2017-03-03 19:36:33 +0300

    Added image to models

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=120)
     subtitle = models.CharField(max_length=120, blank=True)
-    #image
+    image_url = models.CharField(max_length= 300, blank=True)
     text = models.TextField()
     publication_date = models.DateTimeField(auto_now_add=True)
 
