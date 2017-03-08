3fcc076c779704f4e422360ea3f44089d7cbea60
Yuri Istomin <yura.ist@icloud.com>
2017-03-08 14:48:19 +0300

    created registration and login templates

--- authentification/views.py
+++ authentification/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request, 'authentification/login.html')
+
+
+def registration(request):
+    return render(request, 'authentification/registration.html')
--- yuraist/urls.py
+++ yuraist/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from .views import main
-from authentification.views import login
+from authentification.views import login, registration
 
 urlpatterns = [
     # Examples:
@@ -12,4 +12,5 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls')),
     url(r'^info/', include('info.urls')),
     url(r'^login/$', login, name='login'),
+    url(r'^registration/$', registration, name='registration'),
 ]
