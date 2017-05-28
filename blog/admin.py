bd24d1867e4f3a297991cf6746f19c0a08300334
Yuri Istomin <yura.ist@icloud.com>
2017-05-28 10:23:51 +0300

    The code tag style has been modified

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-
     list_display = ('title', 'subtitle', 'publication_date',)
 
 admin.site.register(Post, PostAdmin)
