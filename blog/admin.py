a6a088fee8d947789286f19170d04398690205a6
Yuri Istomin <yura.ist@icloud.com>
2017-05-28 16:44:02 +0300

    Article tags was added

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Tag
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'subtitle', 'publication_date',)
 
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Tag, TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return u'%s' % self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=120)
     subtitle = models.CharField(max_length=120, blank=True)
-    image_url = models.CharField(max_length= 300, blank=True)
+    image_url = models.CharField(max_length=300, blank=True)
     text = models.TextField()
     publication_date = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return self.title
